19f9ea0571c7861cfa697e888d5d567548d813c5
Philip Chen <philipandrewchen@gmail.com>
2023-01-23 23:13:45 -0600

    essentailly done

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,46 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
+app.get("/", (req, res) => {
+  res.send("Pigs are cool.");
+});
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
+// // parse requests of content-type - application/json
+// app.use(bodyParser.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// // parse requests of content-type - application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-// simple route
-app.get("/api/", (req, res) => {
-  res.json({ message: "This is the PiggyCanvas backend." });
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+  },
+});
+
+io.sockets.on("connection", (socket) => {
+  console.log(socket.id + " is connected.");
+
+  socket.on("drawPig", (data) => {
+    socket.broadcast.emit("receiveDrawPig", data);
+  });
+
+  socket.on("reset", () => {
+    socket.broadcast.emit("receiveReset");
+  });
+
+  socket.on("disconnect", () => {
+    console.log(socket.id + " disconnected.");
+  });
 });
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
--- frontend/canvas.js
+++ frontend/canvas.js
@@ -1,16 +1,107 @@
 let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
 let pigApple = document.getElementById("pigApple");
 let pigLine = document.getElementById("pigLine");
 let pigLG = document.getElementById("pigLG");
 let sizeMenu = document.getElementById("sizeMenu");
 let reset = document.getElementById("reset");
 
-const pigs = ["staticpig-apple.png", "staticpig-line.png", "staticpig-lg.png"];
-const pigSize = [25, 50, 75];
+const pigs = [
+  "static/pig-apple.png",
+  "static/pig-line.png",
+  "static/pig-lg.png",
+];
+const pigSize = [20, 40, 75, 120];
 
 let currentPig = 0;
-let currentSize = 1;
+let prev = {};
+let drawing = false;
 
-console.log(sizeMenu.value);
+let currentImg = new Image();
+currentImg.src = pigs[currentPig];
 
-// let socket = io.connect("http://127.0.0.1:8080");
+pigApple.onclick = () => {
+  currentPig = 0;
+  currentImg.src = pigs[currentPig];
+};
+
+pigLine.onclick = () => {
+  currentPig = 1;
+  currentImg.src = pigs[currentPig];
+};
+
+pigLG.onclick = () => {
+  currentPig = 2;
+  currentImg.src = pigs[currentPig];
+};
+
+reset.onclick = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  socket.emit("reset");
+};
+
+let draw = (x, y, img, size) => {
+  console.log("drawing a pig");
+  ctx.drawImage(
+    img,
+    x - pigSize[size] / 2 - 20, // pigs have a 20px margin?!!
+    y - pigSize[size] / 2 - 20,
+    pigSize[size],
+    pigSize[size]
+  );
+};
+
+let drawInterval = (e) => {
+  let canRun = true;
+  return (e) => {
+    if (canRun) {
+      draw(e.pageX, e.pageY, currentImg, sizeMenu.value);
+      socket.emit("drawPig", {
+        x: e.pageX,
+        y: e.pageY,
+        imgIdx: currentPig,
+        size: sizeMenu.value,
+      });
+      canRun = false;
+      setTimeout(() => {
+        canRun = true;
+      }, 50);
+    }
+  };
+};
+
+let myDrawInterval = drawInterval();
+
+// turn on drawing
+canvas.onmousedown = (e) => {
+  drawing = true;
+  draw(e.pageX, e.pageY, currentImg, sizeMenu.value);
+  socket.emit("drawPig", {
+    x: e.pageX,
+    y: e.pageY,
+    imgIdx: currentPig,
+    size: sizeMenu.value,
+  });
+};
+
+canvas.onmouseup = () => {
+  drawing = false;
+};
+// when mouse moves
+canvas.onmousemove = (e) => {
+  if (drawing) {
+    myDrawInterval(e);
+  }
+};
+
+let socket = io.connect("http://127.0.0.1:8080");
+
+socket.on("receiveDrawPig", (data) => {
+  img = new Image();
+  img.src = pigs[data.imgIdx];
+  draw(data.x, data.y, img, data.size);
+});
+
+socket.on("receiveReset", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
